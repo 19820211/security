fd7fbbe26ee4196fffe4bda1bc3223de41c44a38
Grzegorz Stepien <Grzegorz>
2019-06-29 13:28:35 +0200

    Added User Service Test

--- src/main/java/com/example/security2/service/UserService.java
+++ src/main/java/com/example/security2/service/UserService.java
@@ -38,5 +38,6 @@ public class UserService {
         user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
         userRepository.save(user);
     }
+    // test jednostkowy - problem tutaj
 
 }
